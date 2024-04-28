56da329683a5fbd8f0e13537cb72081d37493e9e
azamkhan1997 <azamkhanyousafzai35@gmail.com>
2024-04-28 10:01:37 +0100

    initial commit 2

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 export const api = axios.create({
-  // baseURL: "https://emoji-picker-jtqw.onrender.com"
-  baseURL: "http://localhost:3500",
+  baseURL: "https://emoji-login.onrender.com"
+  // baseURL: "http://localhost:3500",
 });
